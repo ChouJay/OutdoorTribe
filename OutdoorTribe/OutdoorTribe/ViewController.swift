a25aa24f930eab7ca373b46d47da2606d8101a14
Chou <qwe159515@gmail.com>
2022-06-15 14:04:38 +0800

    Build project

---
+++ OutdoorTribe/OutdoorTribe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OutdoorTribe
+//
+//  Created by Jay Chou on 2022/6/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OutdoorTribe/Pods/Target Support Files/Pods-OutdoorTribe/Pods-OutdoorTribe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OutdoorTribe : NSObject
+@end
+@implementation PodsDummy_Pods_OutdoorTribe
+@end
