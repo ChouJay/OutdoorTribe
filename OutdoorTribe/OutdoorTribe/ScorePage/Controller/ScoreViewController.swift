d7c6b10778b3cc6a46258dd4f2e910a7ba45b0f6
Chou <qwe159515@gmail.com>
2022-06-29 10:58:19 +0800

    Temp commit point

---
+++ OutdoorTribe/OutdoorTribe/ScorePage/Controller/ScoreViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScoreViewController.swift
+//  OutdoorTribe
+//
+//  Created by Jay Chou on 2022/6/29.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
